14fdee8b3c152dd3c9da30cca6f0481c9b92b093
Le Minh Hieu <hieubeo0612@gmail.com>
2021-03-22 11:18:22 +0700

    Check_point_ss5

---
+++ ss5/app.js
@@ -0,0 +1,13 @@
+let randomNumber=Math.random();
+console.log(randomNumber);
+let arr=[1,2,3,4,5,6,7,8,9,10];
+randomElement=arr[Math.floor(Math.random()*arr.length)];
+console.log(randomElement);
+
+/*
+- Web về xây dựng
++ menu ở trên cùng và sẽ dính khi kéo xuống gồm 4 tab
++ triển khai 4 tab thành 4 phần
++ 1 người làm html ,thu thập thông tin cho page , code queries(dùng cho nhiều nền tảng)
++ 1 người code css
+*/
